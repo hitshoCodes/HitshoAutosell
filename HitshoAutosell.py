ca6dd3185073d0c11ad14f4b34bb74d674b4eb23
hitshoCodes <134806168+hitshoCodes@users.noreply.github.com>
2023-07-28 09:59:42 +0200

    HitshoAutosell.py

--- HitshoAutosell.py
+++ HitshoAutosell.py
@@ -1,1 +1,8 @@
-print(’Nothing here right now!’)
+import time
+import sys
+
+print("Work in progress, check back later!")
+print("Closing in 5 seconds...")
+time.sleep(5)
+
+sys.exit()  # This will close the program
